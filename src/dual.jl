188b182fd8accb3a7ee8add1ec6797c9d197dfee
navaxel <axel.navarro@polytechnique.edu>
2024-01-09 19:07:22 +0100

    parser + dual resolution

---
+++ src/dual.jl
@@ -0,0 +1,107 @@
+function dual_resolution(g::Graph)
+    n = g.n
+    s = g.s
+    t = g.t
+
+    model = Model(CPLEX.Optimizer)
+
+    @variable(model, x[1:n,1:n] >= 0, binary=true)
+    @variable(model, alpha >= 0)
+    @variable(model, beta[1:n, 1:n] >= 0)
+    @variable(model, gamma >= 0)
+    @variable(model, sigma[1:n] >= 0)
+
+    #Constraint 0
+    for i = 1:n
+        for j = 1:n
+            if g.d[i,j] == 0
+                @constraint(model, x[i,j] == 0)
+                @constraint(model, beta[i,j] == 0)
+            end
+        end
+    end
+
+    #Constraint 1 & 2
+    for i = 1:n
+        if i != s && i != t
+            @constraint(model, sum(x[i,j] for j = 1:n if g.d[i,j] != 0) == sum(x[j,i] for j = 1:n if g.d[i,j] != 0))
+        end
+    end
+
+    #Constraint 3
+    @constraint(model, sum(x[i,s] for i = 1:n if g.d[i,s] != 0) == 0)
+
+    #Constraint 4
+    @constraint(model, sum(x[s,j] for j = 1:n if g.d[s,j] != 0) == 1)
+
+    #Constraint 5
+    @constraint(model, sum(x[i,t] for i = 1:n if g.d[i,t] != 0) == 1)
+
+    #Constraint 6
+    @constraint(model, sum(x[t,j] for j = 1:n if g.d[t,j] != 0) == 0)
+
+    #Constraint 7
+    @constraint(model, sum(x[i,j]*(g.p[i] + g.p[j]) for i = 1:n for j = 1:n if g.d[i,j] != 0) + 2*sum(sigma[i] for i = 1:n) + gamma*g.d2 <= 2*g.S - g.p[s] - g.p[t])
+
+    #Constraint 14
+    for i = 1:n
+        if i != s && i != t
+            @constraint(model, g.ph[i]*sum(x[i,j] for j = 1:n if g.d[i,j] != 0) + g.ph[i]*sum(x[j,i] for j = 1:n if g.d[i,j] != 0) <= gamma + sigma[i])
+        end
+    end
+
+    #Constraint 15
+    @constraint(model, g.ph[s]*sum(x[s,j] for j = 1:n if g.d[s,j] != 0) + g.ph[s]<= gamma + sigma[s])
+
+    #Constraint 16
+    @constraint(model, g.ph[t]*sum(x[i,t] for i = 1:n if g.d[i,t] != 0) + g.ph[t] <= gamma + sigma[t])
+
+    #Constraint 17 
+    for i = 1:n
+        for j = 1:n
+            if g.d[i,j] != 0
+                @constraint(model, g.d[i,j]*x[i,j] <= alpha + beta[i,j])
+            end
+        end
+    end
+
+    #Objective
+    @objective(model, Min, sum(g.d[i,j]*x[i,j] for i = 1:n for j = 1:n if g.d[i,j] != 0) + alpha*g.d1 + sum(beta[i,j]*g.D[i,j] for i = 1:n for j = 1:n if g.d[i,j] != 0))
+
+    optimize!(model)
+
+    feasibleSolutionFound = primal_status(model) == MOI.FEASIBLE_POINT
+    isOptimal = termination_status(model) == MOI.OPTIMAL
+
+    if feasibleSolutionFound
+        path = [s]
+        path_weight = g.p[s]
+        i = s
+        while i != t
+            for j = 1:n
+                if value(x[i,j]) == 1
+                    push!(path, j)
+                    path_weight += g.p[j]
+                    i = j 
+                    break
+                end
+            end
+        end
+        
+        obj_value = JuMP.objective_value(model)
+        return obj_value, path, path_weight
+        # println(g.name)
+        # println("Objective value : ", JuMP.objective_value(model))
+        # println("Path from ", g.s, " to ", g.t, " : ", path)
+        # println("Path weight : ", path_weight, " <= S = ", g.S)
+    end
+    
+    return nothing, nothing, nothing
+    # for i = 1:n
+    #     for j = 1:n
+    #         if value(x[i,j]) == 1
+    #             println((i,j))
+    #         end
+    #     end
+    # end
+end
---
+++ src/graph.jl
@@ -0,0 +1,69 @@
+mutable struct Graph
+    name                ::String
+    n                   ::Int
+    s                   ::Int
+    t                   ::Int
+    S                   ::Int
+    d1                  ::Int
+    d2                  ::Int
+    p                   ::Vector{Int}
+    ph                  ::Vector{Int}
+    d                   ::Matrix{Int}
+    D                   ::Matrix{Float64}
+end
+
+function parse_file(file_name::String)::Graph
+    # Define variables to store data
+    n = 0
+    s = 0
+    t = 0
+    S = 0
+    d1 = 0
+    d2 = 0
+    p = []
+    ph = []
+    d = Matrix{Int}(undef, 0, 0)
+    D = Matrix{Float64}(undef, 0, 0)
+
+    # Open the file for reading
+    file_path = "data/" * file_name
+    file = open(file_path, "r")
+
+    # Read each line from the file
+    for line in eachline(file)
+        # Split the line into tokens based on '=' or whitespace
+        tokens = split(line, " = ")
+
+        # Extract data based on the structure
+        if tokens[1] == "n"
+            n = parse(Int, tokens[2])
+            d = zeros(Int, n, n)
+            D = zeros(Float64, n, n)
+        elseif tokens[1] == "s"
+            s = parse(Int, tokens[2])
+        elseif tokens[1] == "t"
+            t = parse(Int, tokens[2])
+        elseif tokens[1] == "S"
+            S = parse(Int, tokens[2])
+        elseif tokens[1] == "d1"
+            d1 = parse(Int, tokens[2])
+        elseif tokens[1] == "d2"
+            d2 = parse(Int, tokens[2])
+        elseif tokens[1] == "p"
+            p = parse.(Int, split(tokens[2][2:end-1], ","))
+        elseif tokens[1] == "ph"
+            ph = parse.(Int, split(tokens[2][2:end-1], ","))
+        elseif tokens[1] != "Mat"
+            i, j, v, V = split(tokens[1][1:end-1], " ")
+            i, j, v = parse.(Int, [i,j,v])
+            V = parse(Float64, V)
+            d[i,j] = v
+            D[i,j] = V
+        end
+    end
+
+    # Close the file when done
+    close(file)
+
+    return Graph(file_name, n, s, t, S, d1, d2, p, ph, d, D)                  
+end 
